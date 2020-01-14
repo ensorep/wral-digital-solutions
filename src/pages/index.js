a2201951208f990b0ae6d9c01aaaaa25675ef101
rpeterson <rpeterson@cbcits-mbp-2.cbc.corp>
2020-01-14 13:15:59 -0500

    create mobile menu & toggling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+
+import MobileMenu from '../components/mobile-menu'
 import logo from '../images/logo.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faEquals} from '@fortawesome/free-solid-svg-icons'
+
+
+const toggleMenu = () => {
+  const menu = document.getElementById('mobile-menu');
+  const state = getComputedStyle(menu).getPropertyValue('--mobile-menu-offset');
+
+  if(state !== '0') {
+    menu.style.setProperty('--mobile-menu-offset', '0');
+    menu.style.setProperty('--menu-overlay-opacity', '1');
+  } else {
+    menu.style.setProperty('--mobile-menu-offset', '-70vw');
+    menu.style.setProperty('--menu-overlay-opacity', '0');
+  }
+}
+
 export default () => (
     <div id="app">
-      <FontAwesomeIcon id="menu" icon={faEquals}/>
+      <FontAwesomeIcon onClick={toggleMenu} id="icon-menu" icon={faEquals}/>
+      <MobileMenu/>
       <div className="overlay"> </div>
       <img className="logo" src={logo} alt="logo"/>
       <span className="award-winning">award winning</span>
